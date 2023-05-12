0c3a8d1bb2f863b336ce2b850844731f2844107e
nasp-tech <00120319@uca.edu.sv>
2023-05-11 23:43:49 -0600

    :fire: Add: Home Snake Game

---
+++ src/snake_moving_game/Snake_Moving_Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package snake_moving_game;
+
+/**
+ *
+ * @author Nathy
+ */
+public class Snake_Moving_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
