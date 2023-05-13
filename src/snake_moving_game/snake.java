a2bd54660dcf183f121e15903475db165236683a
Natalia Solorzano <solorzanonatalia26@gmail.com>
2023-05-12 21:33:34 -0600

    :fire: Add: Move Forward

--- src/snake_moving_game/snake.java
+++ src/snake_moving_game/snake.java
@@ -2,11 +2,12 @@ package snake_moving_game;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 import java.util.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class snake extends JFrame implements KeyListener, Runnable{
+public class snake extends JFrame implements KeyListener, Runnable {
 
     JPanel p1, p2;
     JButton[] lb = new JButton[200];
@@ -147,17 +148,89 @@ public class snake extends JFrame implements KeyListener, Runnable{
         myt.start();
     }
 
-    void growup(){
+    void growup() {
         lb[gu] = new JButton();
         lb[gu].setEnabled(false);
         p1.add(lb[gu]);
-        int a = 10 + (10*r.nextInt(48));
-        int b = 10 + (10*r.nextInt(23));
-        lbx[gu]=a;
-        lby[gu]=b;
+        int a = 10 + (10 * r.nextInt(48));
+        int b = 10 + (10 * r.nextInt(23));
+        lbx[gu] = a;
+        lby[gu] = b;
         lb[gu].setBounds(a, b, 10, 10);
         gu++;
     }
+
+    void moveForward() {
+        for (int i = 0; i < gu; i++) {
+            lbp[i] = lb[i].getLocation();
+        }
+
+        lbx[0] += directionx;
+        lby[0] += directiony;
+        lb[0].setBounds(lbx[0], lby[0], 10, 10);
+
+        for (int i = 1; i < gu; i++) {
+            lb[i].setLocation(lbp[i - 1]);
+
+        }
+
+        if (lbx[0] == x) {
+            lbx[0] = 10;
+        } else if (lbx[0] == 0) {
+            lbx[0] = x - 10;
+        } else if (lby[0] == y) {
+            lby[0] = 10;
+        } else if (lby[0] == 0) {
+            lby[0] = y - 10;
+        }
+
+        if (lbx[0] == lbx[gu - 1] && lby[0] == lby[gu - 1]) {
+            food = false;
+            score += 5;
+            t.setText("Score" + score);
+            if (score % 50 == 0 && boundsflag) {
+                p1.add(boundsfood);
+                boundsfood.setBounds((10 * r.nextInt(50)), (10 * r.nextInt(25)), 15, 15);
+                bfp = boundsfood.getLocation();
+                boundsflag = false;
+            }
+        }
+
+        if (boundsflag == false) {
+            if (bfp.x <= lbx[0] && bfp.y <= lby[0] && bfp.x + 10 >= lbx[0] && bfp.y + 10 >= lby[0]) {
+                p1.remove(boundsfood);
+                score += 100;
+                t.setText("Score==>" + score);
+                boundsflag = true;
+            }
+        }
+        
+        if(food == false){
+            growup();
+            food = true;
+        }
+        
+        else{
+            lb[gu-1].setBounds(lbx[gu-1], lby[gu-1], 10, 10);            
+        }
+
+        for(int i = 1; i < gu; i++){
+            if(lbp[0]==lbp[i]){
+                t.setText("Game over  -" + score);
+                try {
+                    myt.join();
+                } catch (Exception e) {
+                }
+            }
+        }
+        
+        p1.repaint();
+        show();
+                
+    }
     
     
+    
+    
+
 }
